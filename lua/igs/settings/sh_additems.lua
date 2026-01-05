fb7529acf8687222447538c0bdb9b591c9e08ced
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-01-05 15:07:00 +0300

    More VIPs + better networking

--- lua/igs/settings/sh_additems.lua
+++ lua/igs/settings/sh_additems.lua
@@ -1,17 +1,43 @@
-local vip = IGS("VIP на 30 дней", "vip_month")
-vip:SetPrice(250)
-vip:SetTerm(30)
-vip:SetNetworked(true)
-vip:SetIcon("icons/fa32/user-plus.png", "material")
-vip:SetCategory("Группы")
-vip:SetDescription([[
-Дает эксклюзивные возможности на сервере:
+local vip_groups = {}
 
-Увеличенные вдвое лимиты на спавны
-Возможность спавнить запрещенное оружие и NPC
-Возможность установить пользовательский фон своему профилю в ТАБ-е
-Доступ к резервному слоту на сервере
-Доступ к Starfall и PAC3 до 10 часов
+local function CreateVIP(printname, classname, time, price, discountfrom)
+	local vip = IGS(printname, classname)
+	vip:SetTerm(time)
+	vip:SetPrice(price)
+	vip:SetDiscountedFrom(discountfrom)
+	vip:SetIcon("icons/fa32/user-plus.png", "material")
+	vip:SetCategory("Группы")
+	vip:SetDescription([[
+	Дает эксклюзивные возможности на сервере:
 
-Список будет пополнятся]]
-)
+	Увеличенные вдвое лимиты на спавны
+	Возможность спавнить запрещенное оружие и NPC
+	Возможность установить пользовательский фон своему профилю в ТАБ-е
+	Доступ к резервному слоту на сервере
+	Доступ к Starfall и PAC3 до 10 часов
+
+	Список будет пополнятся]]
+	)
+
+	vip:SetOnActivate(function(ply)
+		ply:SetNW2Bool("CB_VIP", true)
+	end)
+
+	vip:SetCanActivate(function(ply)
+		if IGS.PlayerHasOneOf(ply, vip_groups) then
+			return "У вас уже действует эта услуга"
+		end
+	end)
+
+	table.insert(vip_groups, vip)
+end
+
+CreateVIP("VIP на 30 дней", "vip_30", 30, 250)
+CreateVIP("VIP на 60 дней", "vip_60", 60, 475, 500)
+CreateVIP("VIP на 90 дней", "vip_90", 90, 715, 750)
+
+if SERVER then
+	hook.Add("IGS.PlayerPurchasesLoaded", "ClassicVox_VIP", function(ply)
+		ply:SetNW2Bool("CB_VIP", IGS.PlayerHasOneOf(ply, vip_groups) and true or nil)
+	end, POST_HOOK)
+end
