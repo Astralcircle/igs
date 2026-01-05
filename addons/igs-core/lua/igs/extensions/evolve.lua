402e320e101b8fc7bc2b62f73cef518fb3662336
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-01-05 06:13:40 +0300

    Remove all shitty extensions

--- addons/igs-core/lua/igs/extensions/evolve.lua
+++
@@ -1,9 +0,0 @@
-local ITEM = MT_IGSItem
-
-function ITEM:SetEvolveRank(rank)
-	return self:SetInstaller(function(pl)
-		evolve.PlayerInfo[pl:UniqueID()]["Rank"] = rank
-	end):SetValidator(function(pl)
-		return evolve.PlayerInfo[pl:UniqueID()]["Rank"] == rank
-	end):SetMeta("ev_rank", rank)
-end
