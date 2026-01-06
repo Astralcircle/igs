5ff92266c3913d79b66e8d5de22f60c1b6ea3e11
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-01-06 22:48:12 +0300

    Make it a little more expensive

--- lua/igs/settings/sh_additems.lua
+++ lua/igs/settings/sh_additems.lua
@@ -32,9 +32,9 @@ local function CreateVIP(printname, classname, time, price, discountfrom)
 	table.insert(IGS.VIPGroups, vip)
 end
 
-CreateVIP("VIP на 30 дней", "vip_30", 30, 250)
-CreateVIP("VIP на 60 дней", "vip_60", 60, 475, 500)
-CreateVIP("VIP на 90 дней", "vip_90", 90, 675, 750)
+CreateVIP("VIP на 30 дней", "vip_30", 30, 275)
+CreateVIP("VIP на 60 дней", "vip_60", 60, 525, 550)
+CreateVIP("VIP на 90 дней", "vip_90", 90, 750, 825)
 
 if SERVER then
 	local steamid_checks = {}
