13a463769002ed2834d570284c9bd146eac37fba
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-02-20 19:38:56 +0300

    Silent errors that i can't fix

--- lua/igs/modules/extra/error_logging_sv.lua
+++ lua/igs/modules/extra/error_logging_sv.lua
@@ -1,13 +1,13 @@
 hook.Add("IGS.OnApiError", "LogError", function(sMethod, error_uid, tParams)
 	if error_uid == "http_error" then
 		IGS.prints(Color(255, 0, 0), "", "CEPBEPA GMD BPEMEHHO HE9OCTynHbI. y}{e PEWAEM nPO6JIEMy")
+	else
+		local sparams = "\n"
+
+		for k, v in pairs(tParams) do
+			sparams = sparams .. ("\t%s = %s\n"):format(k,v)
+		end
+
+		ErrorNoHaltWithStack(string.format("IGS API Error:\nMethod: %s\nError: %s\nParams: %s", sMethod, error_uid, sparams))
 	end
-
-	local sparams = "\n"
-
-	for k, v in pairs(tParams) do
-		sparams = sparams .. ("\t%s = %s\n"):format(k,v)
-	end
-
-	ErrorNoHaltWithStack(string.format("IGS API Error:\nMethod: %s\nError: %s\nParams: %s", sMethod, error_uid, sparams))
 end)
